cea3bbf8ff09315cae27be604ea788969c5ee0c8
Gaston de la Fare <gastondelafare@gmail.com>
2024-11-26 09:46:23 -0300

    partial front-end improvements

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+console.log("i am here");
+
+const letter = document.querySelectorAll(".letter");
+
+letter.forEach((letter) => {
+    letter.addEventListener("click", (event) => {
+        console.log("clicked");
+    });    
+});
